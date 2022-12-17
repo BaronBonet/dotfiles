10b5bbf6336d0306a35b18d3c2065911e2ce0c76
ericcbonet <ericcbonet@gmail.com>
2022-12-17 12:14:41 +0100

    added nvim config

--- run_after_install.sh
+++ run_after_install.sh
@@ -2,8 +2,7 @@
 
 # fzf for ctrl+r
 folder="~/.fzf"
-if ! git clone --depth 1 https://github.com/junegunn/fzf.git "${folder}" 2>/dev/null && [ -d "${fol
-der}" ]; then
+if ! git clone --depth 1 https://github.com/junegunn/fzf.git "${folder}" 2>/dev/null && [ -d "${folder}" ]; then
     echo "Failed to clone fzf because directory ${fzfDirectory} exists"
 else
     yes  | ~/.fzf/install
@@ -21,4 +20,12 @@ else
 
 	# NVim
 	brew install neovim
+
+	# Plugin manager vim-plug: https://github.com/junegunn/vim-plug
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+		       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+	echo "Vim plug installed be sure to run ':PlugInstall' next time you use neovim."
+
+	echo "Trying to install coco, you may need to install yarn for this to work"
+	cd ~/.config/.local/share/nvim/plugged/coco.nvim && yarn install
 fi
