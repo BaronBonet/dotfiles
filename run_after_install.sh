5f4e01151c3aa836b595ed8a18a415f02f0acf84
ericcbonet <ericcbonet@gmail.com>
2022-12-17 11:48:43 +0100

    fixed install script

--- run_after_install.sh
+++ run_after_install.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
 # fzf for ctrl+r
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes  | ~/.fzf/install 
+folder="~/.fzf"
+if ! git clone --depth 1 https://github.com/junegunn/fzf.git "${folder}" 2>/dev/null && [ -d "${fol
+der}" ]; then
+    echo "Failed to clone fzf because directory ${fzfDirectory} exists"
+else
+    yes  | ~/.fzf/install
+fi
 
-# Diff so fancy
-brew install diff-so-fancy
 
-# bat for nice cat highlighting
-brew install bat
+if ! brew update 2>/dev/null; then
+    echo "Something is wrong with brew, perhaps it is not installed or you don't have permission."
+else
+	# Diff so fancy
+	brew install diff-so-fancy
 
-# NVim
-brew install neovim
+	# bat for nice cat highlighting
+	brew install bat
 
+	# NVim
+	brew install neovim
+fi
