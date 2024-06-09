d608a70e03577654b8c3e26bad2585d06381a87c
Eric Bonet <ericcbonet@gmail.com>
2024-06-09 23:23:05 +0200

    fix tmux install

--- run_onchange_2-configure-tmux.sh
+++ run_onchange_2-configure-tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/.tmux/plugins/tpm/scripts/install_plugins.sh
+"$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh"
 # Install tms for convenient tmux session
 if ! command -v tms >/dev/null 2>&1; then
 	echo "Installing tms for tmux session management"
